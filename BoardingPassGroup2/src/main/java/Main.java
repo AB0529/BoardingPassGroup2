de0cdafb74a1a2ecba0f1c2b1553beebe566966c
AB0529 <supercrab115@gmail.com>
2022-05-02 13:47:22 -0400

    Updated main

--- BoardingPassGroup2/src/main/java/Main.java
+++ BoardingPassGroup2/src/main/java/Main.java
@@ -8,26 +8,6 @@ public class Main {
         String dbPath = Main.class.getResource("BoardingPassesDEV.db").getPath();
         Database db = new Database("jdbc:sqlite:" + dbPath);
 
-        db.createTable();
-        try {
-            db.insertPass(new BoardingPass(
-                    1,
-                    "May 19th",
-                    "Texas",
-                    "Ohio",
-                    "AMOGUS",
-                    "amogus@sus.com",
-                    "1234",
-                    "male",
-                    2.5,
-                    "NOW!!",
-                    420.69));
-        } catch (Exception e) {
-            System.out.println("Cannot add duplicate pass numbers");
-        }
-        System.out.println(db.findPassByNumber(1));
-
-        db.updatePassDestinationByNumber(1, "Florida");
-        System.out.println(db.findPassByNumber(1));
+        // Add some code here!
     }
 }
