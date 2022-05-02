756ed3f504c7e6c04cd744d690e65b8b24a26e74
AB0529 <supercrab115@gmail.com>
2022-05-02 11:08:02 -0400

    Removed unsued import

--- BoardingPassGroup2/src/main/java/Main.java
+++ BoardingPassGroup2/src/main/java/Main.java
@@ -1,7 +1,5 @@
 import database.Database;
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         String dbPath = Main.class.getResource("BoardingPasses.db").getPath();
