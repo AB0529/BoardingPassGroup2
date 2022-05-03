cf6b8d77dfd36d5c5cae38cca3b44cea9cbc4bbb
AB0529 <supercrab115@gmail.com>
2022-05-03 11:53:28 -0400

    Orginzed things and made user input be gettable from GUIController class

--- BoardingPassGroup2/src/main/java/Main.java
+++ BoardingPassGroup2/src/main/java/Main.java
@@ -1,12 +1,22 @@
 import controllers.DatabaseController;
+import controllers.GUIController;
+import controllers.models.Input;
 
 public class Main {
     public static void main(String[] args) {
+        // Initialize database
         String dbPath = Main.class.getResource("BoardingPassesDEV.db").getPath();
         DatabaseController db = new DatabaseController("jdbc:sqlite:" + dbPath);
 
         db.createTable();
 
-        // Add some code here!
+        // Initialize GUI
+        GUIController guiController = new GUIController();
+
+        // Grab the user input
+        Input input = guiController.getUserInput();
+
+        if (input != null)
+            System.out.println(input);
     }
 }
